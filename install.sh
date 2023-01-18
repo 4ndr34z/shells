10e5534f815ca381b63291f76ccf31233cbf1ced
Andreas Finstad <andreas@f20.be>
2023-01-18 20:05:54 +0100

    Updated installer

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-# check for pre existing installation --
-if [ -d "/usr/local/share/shells" ] || [ -f "/usr/local/bin/shells" ]
-then
-  echo "[!] detected a prior installation !"
-  echo "[!] please backup '/usr/local/share/shells' and '/usr/local/bin/shells' and then rerun; bash install.sh"
-  echo "[!] exiting."
-  exit
-fi
+
 
 # install --
 echo "[+] creating directory"
@@ -16,8 +9,8 @@ echo "[+] moving 'shells.sh' into /usr/local/share/shells/"
 sudo cp -Rvp ./shells.sh /usr/local/share/shells/
 
 echo "[+] creating wrapper script"
-echo -ne '#!/bin/sh\ncd /usr/local/share/shells/\nexec ./shells.sh "$@"' \
-  | sudo tee -a /usr/local/bin/shells ; echo
+echo -e '#!/bin/sh\ncd /usr/local/share/shells/\nexec ./shells.sh "$@"' \
+  | sudo tee /usr/local/bin/shells ; echo
 
 echo "[+] setting permissions on shells wrapper"
 sudo chmod a+x "/usr/local/bin/shells"
