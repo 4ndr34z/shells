a58a443e55f66eb4a20076b791ae5b683ac283a1
4ndr34z <andreas@f20.be>
2023-01-24 15:25:33 +0100

    Test: Listener

--- shells.sh
+++ shells.sh
@@ -1902,7 +1902,42 @@ fi
                 
             else
             
+                echo
+                yellowprint "Do you wish to listen in a new terminal window [Y/n]"
+                read -r -n 1 ans
+                case $ans in
+            y)
+                 echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT\n" > /tmp/listen && chmod +x /tmp/listen
+                for terminal in "$TERMINAL" x-terminal-emulator qterminal mate-terminal gnome-terminal terminator xfce4-terminal urxvt rxvt termit Eterm aterm roxterm termite lxterminal terminology st lilyterm tilix terminix konsole kitty guake tilda alacritty hyper wezterm; do
+                    if command -v "$terminal" > /dev/null 2>&1
+                    then
+
+                        "$terminal" -e "/tmp/listen"&
+                        break
+                fi
+                done
+                mainmenu
+                ;;
+            n)
+                echo
                 $rlwrap -cAr $nc $prot -lvnp $PORT
+                ;;
+            "")
+                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT\n" > /tmp/listen && chmod +x /tmp/listen
+                for terminal in "$TERMINAL" x-terminal-emulator qterminal mate-terminal gnome-terminal terminator xfce4-terminal urxvt rxvt termit Eterm aterm roxterm termite lxterminal terminology st lilyterm tilix terminix konsole kitty guake tilda alacritty hyper wezterm; do
+                    if command -v "$terminal" > /dev/null 2>&1
+                    then
+
+                        "$terminal" -e "/tmp/listen"&
+                        break
+                fi
+                done
+                mainmenu
+                ;;
+            *)
+                echo
+                ;;
+            esac
                 
             fi
         else
