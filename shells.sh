53285979dc1d6601f0ea76324b512a82470ad2b0
4ndr34z <andreas@f20.be>
2023-01-24 14:25:00 +0100

    Test: Listener

--- shells.sh
+++ shells.sh
@@ -1751,7 +1751,7 @@ fi
                 read -r -n 1 ans
                 case $ans in
             y)
-                 echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT" > /tmp/listen && chmod +x /tmp/listen
+                 echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT\n" > /tmp/listen && chmod +x /tmp/listen
                 for terminal in "$TERMINAL" x-terminal-emulator qterminal mate-terminal gnome-terminal terminator xfce4-terminal urxvt rxvt termit Eterm aterm roxterm termite lxterminal terminology st lilyterm tilix terminix konsole kitty guake tilda alacritty hyper wezterm; do
                     if command -v "$terminal" > /dev/null 2>&1
                     then
@@ -1767,7 +1767,7 @@ fi
                 $rlwrap -cAr $nc $prot -lvnp $PORT
                 ;;
             "")
-                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT" > /tmp/listen && chmod +x /tmp/listen
+                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT\n" > /tmp/listen && chmod +x /tmp/listen
                 for terminal in "$TERMINAL" x-terminal-emulator qterminal mate-terminal gnome-terminal terminator xfce4-terminal urxvt rxvt termit Eterm aterm roxterm termite lxterminal terminology st lilyterm tilix terminix konsole kitty guake tilda alacritty hyper wezterm; do
                     if command -v "$terminal" > /dev/null 2>&1
                     then
@@ -1834,7 +1834,7 @@ fi
                 read -r -n 1 ans
                 case $ans in
             y)
-                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT" > /tmp/listen && chmod +x /tmp/listen
+                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT\n" > /tmp/listen && chmod +x /tmp/listen
                 for terminal in "$TERMINAL" x-terminal-emulator qterminal mate-terminal gnome-terminal terminator xfce4-terminal urxvt rxvt termit Eterm aterm roxterm termite lxterminal terminology st lilyterm tilix terminix konsole kitty guake tilda alacritty hyper wezterm; do
                     if command -v "$terminal" > /dev/null 2>&1
                     then
@@ -1850,7 +1850,7 @@ fi
                 $rlwrap -cAr $nc $prot -lvnp $PORT
                 ;;
             "")
-                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT" > /tmp/listen && chmod +x /tmp/listen
+                echo -en "#!/bin/bash\nsleep 0.5\n$rlwrap -cAr $nc $prot -lvnp $PORT\n" > /tmp/listen && chmod +x /tmp/listen
                 for terminal in "$TERMINAL" x-terminal-emulator qterminal mate-terminal gnome-terminal terminator xfce4-terminal urxvt rxvt termit Eterm aterm roxterm termite lxterminal terminology st lilyterm tilix terminix konsole kitty guake tilda alacritty hyper wezterm; do
                     if command -v "$terminal" > /dev/null 2>&1
                     then
